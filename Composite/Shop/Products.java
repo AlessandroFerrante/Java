bd770cf7aa2720505ac9012c779c59c06cb16673
Alessandro Ferrante <104135680+AlessandroFerrante@users.noreply.github.com>
2024-07-19 13:10:06 +0200

    Add Component-Products (Shop Composite)

---
+++ Composite/Shop/Products.java
@@ -0,0 +1,8 @@
+// Component
+public interface Products {
+    public void show();
+    public float getPrice();
+    public float getWeight();
+    public void addP(Products p);
+    public void removeP(Products p);
+}
